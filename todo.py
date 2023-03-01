e989ff99b3cb7ef1d3592836f36d489c89f20ac3
Kevaughn Heron <kheron@brooklynsteamcenter.org>
2023-03-01 09:25:29 -0500

    ,

---
+++ todo.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+
+app=Flask(__name__)
+
+my_todos=[
+    'Visit Vietnam',
+    'Complete my bachelors'
+]
+
+@app.route('/')
+def index():
+    return render_template(
+        'todo.html.jinja',
+        todos=my_todos
+     )
+
+@app.route('/add', methods=['POST'])
+def add():
+    new_todo= request.form('new_todo')
+    return new_todo
