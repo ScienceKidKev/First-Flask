d2e10c279c869a92c1f3d7f16da715d5fcff2cb6
Kevaughn Heron <kheron@brooklynsteamcenter.org>
2023-03-13 10:52:55 -0400

    .

--- todo.py
+++ todo.py
@@ -25,9 +25,24 @@ def index():
 @app.route('/add', methods=['POST'])
 def add():
     new_todo = request.form['new_todo']
+    cursor=connection.cursor
     cursor.execute(f"INSERT INTO `todos`(`Description`) VALUES ('{new_todo}') ")
     my_todos.append(new_todo)
     return result(('/todo'))
+    
+
+
+@app.route('/complete', methods=['POST'])
+def complete():
+        todo_id = request.form['todo_id']
+
+        cursor=connection.cursor()
+
+        cursor.execute(f'UPDATE `todos` SET `Complete` = 1 WHERE `ID` = {todo_id}')
+
+
+        return redirect('/')
+
 
 
 
@@ -42,4 +57,12 @@ connection= pymysql.connect(
     cursorclass=pymysql.cursors.DictCursor,
     autocommit=True
 )
+
+
+
+
+
+
+
+
 
