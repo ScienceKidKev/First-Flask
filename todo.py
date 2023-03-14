39e93485ed15a9c9ae7416c7308798870514c316
Kevaughn Heron <kheron@brooklynsteamcenter.org>
2023-03-14 10:48:19 -0400

    /

--- todo.py
+++ todo.py
@@ -1,6 +1,23 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
+from flask_httpauth import HTTPBasicAuth
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app=Flask(__name__)
+
+
+app = Flask(__name__)
+auth = HTTPBasicAuth()
+
+users = {
+    "Kevaughn": generate_password_hash("wagwan"),
+}
+
+@auth.verify_password
+def verify_password(username, password):
+    if username in users and \
+            check_password_hash(users.get(username), password):
+        return username
+
 
 my_todos=[
     'Grow a couple more inches',
@@ -23,16 +40,18 @@ def index():
      )
 
 @app.route('/add', methods=['POST'])
+@auth.login_required
 def add():
     new_todo = request.form['new_todo']
-    cursor=connection.cursor
+    cursor=connection.cursor()
     cursor.execute(f"INSERT INTO `todos`(`Description`) VALUES ('{new_todo}') ")
     my_todos.append(new_todo)
-    return result(('/todo'))
+    return redirect('/')
     
 
 
 @app.route('/complete', methods=['POST'])
+@auth.login_required
 def complete():
         todo_id = request.form['todo_id']
 
@@ -57,8 +76,6 @@ connection= pymysql.connect(
     cursorclass=pymysql.cursors.DictCursor,
     autocommit=True
 )
-
-
 
 
 
